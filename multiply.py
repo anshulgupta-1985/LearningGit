6096d874888fc05a2ea8129905851157289843ac
Anshul Gupta <it.anshul1985@gmail.com>
2025-05-05 22:10:20 +0530

    Add multiply.py file

---
+++ multiply.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = 2
+num2 = 4
+
+# Multiply two numbers
+sum = num1 * num2
+
+# Display the multiply
+print('The multiply of all the numbers {0} and {1} is {2}'.format(num1, num2, sum))
